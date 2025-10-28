639b52f5c05e495e8547b99f18ecb90fec9a8b0a
TobiasVentre <56970691+TobiasVentre@users.noreply.github.com>
2025-10-28 19:35:06 -0300

    Add JWT validation middleware and tests

--- CuidarMed+/Program.cs
+++ CuidarMed+/Program.cs
@@ -1,6 +1,7 @@
 using Application.Interfaces;
 using Application.Services;
 using Infraestructure.Command;
+using Infraestructure.Middleware;
 using Infraestructure.Persistence;
 using Infraestructure.Queries;
 using Microsoft.AspNetCore.Builder;
@@ -11,21 +12,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddAuthorization();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Obtenego la cadena de conexión
+// Obtenego la cadena de conexin
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 if (string.IsNullOrEmpty(connectionString))
 {
     // Si esta parte falla, es la causa del error.
-    throw new InvalidOperationException("La cadena de conexión 'DefaultConnection' no fue encontrada en appsettings.json.");
+    throw new InvalidOperationException("La cadena de conexin 'DefaultConnection' no fue encontrada en appsettings.json.");
 }
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString) // Pasa la cadena LEÍDA aquí
+    options.UseSqlServer(connectionString) // Pasa la cadena LEDA aqu
 );
 
 // ========== QUERIES (lectura) - Infrastructure ==========
@@ -49,6 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<JwtValidationMiddleware>();
 
 app.UseAuthorization();
 
